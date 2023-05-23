ad6f1931f430189722fbb85eb08d525dd31c6c28
bdorninger <37180819+bdorninger@users.noreply.github.com>
2023-05-23 13:24:27 +0200

    4 files modified

--- data.ts
+++ data.ts
@@ -1,9 +1,43 @@
+export interface CommentedObject {
+  __COMMENT__?: string;
+}
+
+export interface ViewConfigMeta extends CommentedObject {
+  modelId: string;
+  modelType: string;
+}
+/* export interface ViewDataMeta {
+  insertAt?: string;
+  constributor?: string;
+}*/
+
+export interface ViewData extends CommentedObject {
+  name?: string;
+  viewId?: string;
+  evsModel?: string;
+  insertionPoint?: string;
+  insertAt?: string;
+  position?: number; // sort pos
+  header?: ViewHeaderData[];
+  content?: ViewContentData[];
+}
+
+export type RuntimeViewData = ViewData & { contributor?: string };
+
+export interface ViewHeaderData extends ViewData {}
+
+export interface ViewContentData extends ViewData {}
+
+export interface ViewConfig extends CommentedObject, ViewData {
+  meta?: ViewConfigMeta;
+}
+
 export let sample = {};
 export let foodata = {};
 export let fdata = {};
-export let viewdata = {};
-export let viewsnip = {};
-export let viewsnip3 = {};
+export let viewdata: ViewConfig = {};
+export let viewsnip: ViewConfig = {};
+export let viewsnip3: ViewConfig = {};
 
 export function initData() {
   sample = {
@@ -67,7 +101,7 @@ export function initData() {
     content: [
       {
         viewId: 'evs-panel',
-        insertId: 'myId',
+        insertionPoint: 'myId',
         header: [
           {
             viewId: 'evs-switch',
@@ -94,7 +128,6 @@ export function initData() {
             viewId: 'evs-marker',
             position: 20,
             evsModel: 'nsu=http://engelglobal.com/IMM/AirValve1/;s=do_AirValve',
-            displayName: true,
           },
         ],
       },
@@ -124,13 +157,12 @@ export function initData() {
           {
             viewId: 'evs-marker',
             evsModel: 'nsu=http://engelglobal.com/IMM/AirValve2/;s=do_AirValve',
-            displayName: true,
           },
         ],
       },
       {
         viewId: 'evs-panel',
-        insertId: 'myId2',
+        insertionPoint: 'myId2',
         header: [
           {
             viewId: 'evs-switch',
@@ -144,7 +176,7 @@ export function initData() {
           {
             viewId: 'evs-input-number',
             position: 10,
-            insertId: 'myInnerId',
+            insertionPoint: 'myInnerId',
             evsModel:
               'nsu=http://engelglobal.com/IMM/AirValve3/;s=sv_rActiveTime',
             content: [
@@ -159,7 +191,6 @@ export function initData() {
             viewId: 'evs-marker',
             position: 20,
             evsModel: 'nsu=http://engelglobal.com/IMM/AirValve3/;s=do_AirValve',
-            displayName: true,
           },
         ],
       },
@@ -169,8 +200,8 @@ export function initData() {
   viewsnip = {
     viewId: 'evs-panel',
     name: 'Air valve - blowing',
-    insertId: 'myId2',
-    position: '22',
+    insertAt: 'myId2',
+    position: 22,
     content: [
       {
         viewId: 'evs-input-number',
@@ -189,18 +220,18 @@ export function initData() {
   viewsnip3 = {
     viewId: 'evs-panel',
     name: 'something snippet',
-    // insertId: 'top',
-    position: '22',
+    // insertionPoint: 'top',
+    position: 22,
     content: [
       {
         viewId: 'evs-input-number',
-        insertId: 'myId31',
+        insertAt: 'myId31',
         position: 90,
         evsModel: 'nsu=http://engelglobal.com/IMM/AirValve1/;s=sv_rActiveTime',
       },
       {
         viewId: 'evs-actual-number',
-        insertId: 'myId32',
+        insertAt: 'myId32',
         position: 25,
         evsModel:
           'nsu=http://engelglobal.com/IMM/AirValve1/;s=sv_rActualActiveTime',
--- index.ts
+++ index.ts
@@ -2,7 +2,13 @@ import './style.css';
 
 import { initData } from './data';
 import { basicMerges } from './basic-merges';
+import { ajax } from 'rxjs/ajax';
 
 initData();
 
 basicMerges();
+
+
+const obs$ = ajax.getJSON(`./models/mold1.view.json`)
+
+obs$.subscribe()
